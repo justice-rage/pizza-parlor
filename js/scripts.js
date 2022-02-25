86199dfda4a16a4c4d2d8043b823325e7baa590c
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-25 11:17:24 -0800

    add test 6 passing function - addSize

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@
 // Constructor Function: Pizza
 function Pizza(topping, size) {
   this.topping = [];
-  this.size = size;
+  // this.size = size;
+  this.size = [];
 }
 
 // Function: Pushes customer requested toppings into topping array
@@ -13,8 +14,13 @@ Pizza.prototype.addToppings = function(toppings) {
   this.topping.push(toppings);
 }
 
+// Function: Pushes customer requested size into size array
+Pizza.prototype.addSize = function(pizzaSize) {
+  this.size.push(pizzaSize);
+}
+
 // Variable: Customer pizza order
-const pizzaOrder = new Pizza('', 'large');
+// const testPizzaOrder = new Pizza();
 
 // Function: Order Pricing
 Pizza.prototype.price = function() {
@@ -44,4 +50,77 @@ Pizza.prototype.price = function() {
   return orderTotal;
 }
 
+////////////////////////////////////////////////////
+
 // User Interface 
+$(document).ready(function() {
+  $("form#input-form").submit(function(event) {
+    event.preventDefault();
+    // const pizzaOrder = new Pizza();
+
+    const requestedPizzaSize = $("#requested-size").val();
+    const requestedPizzaToppings = $("input#requested-toppings").val();
+
+    // const addPizzaToppingsToOrder = pizzaOrder.addToppings(requestedPizzaToppings);
+    // const addPizzaSizeToOrder = pizzaOrder.price(requestedPizzaSize);
+
+    // const totalPizzaOrderPrice = roboTranslator(userInput);
+
+    let pizzaOrder = new Pizza();
+    pizzaOrder.addToppings(requestedPizzaToppings);
+    pizzaOrder.addSize(requestedPizzaSize);
+
+    console.log(requestedPizzaSize, requestedPizzaToppings);
+    console.log(pizzaOrder);
+    // console.log(pizzaOrder);
+
+    // $(".result").text(cleanedUserInput);
+  });
+});
+
+// programming language selector example
+// $(document).ready(function(event) {
+
+//   $("form#quiz").submit(function(event) {
+//     const identifyWith = $("#identify-with").val();
+//     const favoriteColor = $("#favorite-color").val();
+//     const preferredOrganization = $("#preferred-organization").val();
+
+//     // unhides ruby language result
+//     if (identifyWith === "precious-gems" && favoriteColor === "red") {
+//       $('#ruby').show();
+//       $("#python").hide();
+//       $("#javascript").hide();
+//     } 
+//     // unhides python language result
+//     else if (identifyWith === "snakes" && favoriteColor === "green") {
+//       $('#python').show();
+//       $("#ruby").hide();
+//       $("#javascript").hide();
+//     } 
+//     // unhides javascript language result
+//     else if (identifyWith === "aesthetics" && favoriteColor === "yellow") {
+//       $('#javascript').show();
+//       $("#python").hide();
+//       $("#ruby").hide();
+//     } else {
+//       $('#javascript').show();
+//       $("#python").hide();
+//       $("#ruby").hide();
+//     }
+//     event.preventDefault();
+//   });
+// });
+
+// mr-robogers-neighborhood example
+// $(document).ready(function() {
+//   $("form#input-form").submit(function(event) {
+//     event.preventDefault();
+//     const userInput = parseInt($("input#user-input").val());
+//     const translatedInput = roboTranslator(userInput);
+//     const userInputStringForm = translatedInput.toString();
+//     const cleanedUserInput = userInputStringForm.replace(/,/g,' ');
+
+//     $(".result").text(cleanedUserInput);
+//   });
+// });
